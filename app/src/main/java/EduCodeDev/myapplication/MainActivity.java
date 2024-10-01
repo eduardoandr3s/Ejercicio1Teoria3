ebbf8d24b724a1b2863c11c0cf84291f58a9f3b9
EduAndr3s <eduardoandres.segoviaroman@cieep.com>
2024-10-01 10:24:17 +0200

    Se agregó funcionalidad al botón cuenta caracteres y se empezó en activity1 la funcionalidad de botón 2.

--- app/src/main/java/EduCodeDev/myapplication/MainActivity.java
+++ app/src/main/java/EduCodeDev/myapplication/MainActivity.java
@@ -1,5 +1,6 @@
 package EduCodeDev.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -18,17 +19,17 @@ frase y otro que diga cuantas palabras tiene esa misma frase.*/
 public class MainActivity extends AppCompatActivity {
 
 
-        private EditText txtFrase;
-        private Button btnCuentaChar;
-        private Button btnCuentaPalabras;
+    private EditText txtFrase;
+    private Button btnCuentaChar;
+    private Button btnCuentaPalabras;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
-        
+
+
         inicializarVistas();
 
         btnCuentaChar.setOnClickListener(new View.OnClickListener() {
@@ -36,11 +37,36 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
 
                 String frase = txtFrase.getText().toString();
+                boolean bool = true;
 
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                Bundle bundle = new Bundle();
+                bundle.putString("FRASE", frase);
+                bundle.putBoolean("BOOL", bool);
 
+                intent.putExtras(bundle);
+                startActivity(intent);
             }
         });
-        
+
+
+        btnCuentaPalabras.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String frase2 = txtFrase.getText().toString();
+                boolean bool2 = true;
+
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                Bundle bundle = new Bundle();
+                bundle.putString("FRASE", frase2);
+                bundle.putBoolean("BOOL", bool2);
+
+                intent.putExtras(bundle);
+                startActivity(intent);
+            }
+        });
+
     }
 
     private void inicializarVistas() {
--- app/src/main/java/EduCodeDev/myapplication/MainActivity2.java
+++ app/src/main/java/EduCodeDev/myapplication/MainActivity2.java
@@ -1,6 +1,8 @@
 package EduCodeDev.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,9 +12,26 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity2 extends AppCompatActivity {
 
+    private TextView txtContador;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        txtContador = findViewById(R.id.txtContadorMain2);
+        Intent intent = getIntent();
+        Bundle bundle = intent.getExtras();
+
+        if (bundle != null) {
+
+            String frase = bundle.getString("FRASE");
+            boolean bool = bundle.getBoolean("BOOL");
+
+            txtContador.setText(String.valueOf(frase.length()));
+        }
+
+
     }
 }
